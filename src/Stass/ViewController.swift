258d97d660abee5a3f7826e3012332755ed2eb7d
Stanislav Shabalin <slalom2001@gmail.com>
2016-10-21 18:15:45 +0300

    Initial

---
+++ src/Stass/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stass
+//
+//  Created by Juris Andersons on 21/10/2016.
+//  Copyright © 2016 Hotelplan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
